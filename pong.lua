b1d396ecb5a80e4d5a14237823616ef74d1a6817
Vitor Martins de Sant'Anna <vitormartins.one@gmail.com>
2017-06-07 05:55:52 -0300

    Game loop simples, draw e eventos de entrada.

--- pong.lua
+++ pong.lua
@@ -1,19 +1,34 @@
 local frames = 60
 local frame = 1000 / frames
+local x, y = 0, 0
 
-function update() {
-}
+function update()
+  
+end
 
-function draw() {
-  canvas:attrColor('black')
+function draw()
+  canvas:attrColor (0,0,0,255);
   canvas:drawRect('fill', 0, 0, canvas:attrSize())
-  canvas:attrFont ('vera', 110,'bold')
-  canvas:drawText('pong')
+  canvas:attrColor (255,255,255,255);
+  canvas:attrFont ('vera', 20,'bold')
+  canvas:drawText(x, y, 'pong')
   canvas:flush()
-}
+end
 
-loop = function () {
+loop = function ()
   update()
   draw()
   event.timer(frame, loop)
-} 
+end
+
+function handler(evt) 
+  if (evt.class == 'ncl') and (evt.type == 'presentation') and (evt.action == 'start') then
+--    draw()
+    loop()
+  end
+  
+  if (evt.class == 'key') and (evt.type == 'press') and (evt.key == 'CURSOR_RIGHT') then
+    x = x + 5
+  end
+end
+event.register(handler)
